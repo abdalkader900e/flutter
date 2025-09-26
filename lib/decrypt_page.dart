93cef67831399501205697a88b9cf6d48fd0b5c0
kira <kira@gmail.com>
2025-09-26 19:45:04 +0300

    feat: implement setState for real-time UI updates

--- lib/decrypt_page.dart
+++ lib/decrypt_page.dart
@@ -12,7 +12,6 @@ class DecryptPage extends StatefulWidget {
 
 
 
-
 class _DecryptPageState extends State<DecryptPage> {
   TextEditingController _controller = TextEditingController();
   String result = "";
