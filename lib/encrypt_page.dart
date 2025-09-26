984b0746455bc499144c8f8563b0c81085f80945
kira <kira@gmail.com>
2025-09-26 19:51:15 +0300

    organize imports for material and crypto_helper

--- lib/encrypt_page.dart
+++ lib/encrypt_page.dart
@@ -20,6 +20,7 @@ class EncryptPage extends StatefulWidget {
 
 
 
+
 class _EncryptPageState extends State<EncryptPage> {
   TextEditingController _controller = TextEditingController();
   String result = "";
