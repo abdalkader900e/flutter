7caec669b52de4adc801a2687f7e581688de9098
kira <kira@gmail.com>
2025-09-26 19:50:16 +0300

    display encrypted result as selectable text

--- lib/encrypt_page.dart
+++ lib/encrypt_page.dart
@@ -13,6 +13,7 @@ class EncryptPage extends StatefulWidget {
 
 
 
+
 class _EncryptPageState extends State<EncryptPage> {
   TextEditingController _controller = TextEditingController();
   String result = "";
