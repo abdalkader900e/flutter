b142d0fa72bf424c1d72a4c6c7b5aeb20ebe0c62
kira <kira@gmail.com>
2025-09-26 19:49:50 +0300

    implement text input field with outline border

--- lib/encrypt_page.dart
+++ lib/encrypt_page.dart
@@ -10,6 +10,7 @@ class EncryptPage extends StatefulWidget {
 
 
 
+
 class _EncryptPageState extends State<EncryptPage> {
   TextEditingController _controller = TextEditingController();
   String result = "";
