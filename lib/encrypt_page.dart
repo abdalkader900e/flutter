50ab040da966f06913d392e57a0c900160d7bcae
kira <kira@gmail.com>
2025-09-26 19:50:41 +0300

    add arabic labels for encryption interface

--- lib/encrypt_page.dart
+++ lib/encrypt_page.dart
@@ -16,6 +16,7 @@ class EncryptPage extends StatefulWidget {
 
 
 
+
 class _EncryptPageState extends State<EncryptPage> {
   TextEditingController _controller = TextEditingController();
   String result = "";
